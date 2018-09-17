3c1ea7bff2ee6fe7532db94adc24b157ffaf5e4a
Cristhian Duran <durancristhian@gmail.com>
2018-09-17 08:02:50 -0300

    add positions

--- juego.js
+++ juego.js
@@ -17,14 +17,15 @@ const notReactDOM = {
     }
 };
 
-const renderCell = logo => {
-    return /* html */ `<div class="cell flex items-center justify-center pa2 pointer" onclick="this.classList.toggle('active');">
-        <img src="${logo}" class="db mh-100 mw-100" />
+const renderCell = (logo, position) => {
+    return /* html */ `<div class="cell flex flex-column items-center justify-center overflow-hidden pa1 pointer relative" onclick="this.classList.toggle('active');">
+        <img src="${logo}" class="db mh-100" />
+        <span class="black-30 dib f7 pa1">${position}</span>
     </div>`;
 }
 
 const renderLogos = (logos) => {
     return /* html */ `<div class="cellsContainer flex flex-wrap items-center vh-100 w-100">
-        ${logos.map(logo => renderCell(logo)).join('')}
+        ${logos.map((logo, index) => renderCell(logo, index + 1)).join('')}
     </div>`;
 }
